a8243ee809d18d196045439914c6db7924b99a9d
COR Students <corstudents@Matts-MBP-8.localdomain>
2018-10-26 11:34:54 -0400

    calculator project

---
+++ script.js
@@ -0,0 +1,5 @@
+// $(".button").click(function(event) {
+//     let thingClicked = this.innerHTML
+//     console.log("you clicked: ", thingClicked)
+//      $("#currentValue").html(thingClicked)
+//    })
