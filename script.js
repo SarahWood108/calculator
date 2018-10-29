9dcce77ac170c3e9e4091d20d65fa07e659de16e
COR Students <corstudents@Matts-MBP-8.localdomain>
2018-10-29 11:46:35 -0400

    calculator project

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-$(".btn").click(function(event) {
-    let thingClicked = this.innerHTML
-    console.log("you clicked: ", thingClicked)
-     $("#currentValue").html(thingClicked)
-   })
+
+// function getCurrenValue(num1, num2){
+// let firstValue = ""
+// let secondValue = ""
+// let operator = ""
+// document.getElementById("firstValue", "secondValue").innerHTML
+// }
+
+// $(".btn").click(function(event) {
+//     let thingClicked = this.innerHTML
+//     console.log("you clicked: ", thingClicked)
+//      $("#currentValue").html(thingClicked)
+//    })
+
+function currentValue(){
+  
+}
+
+
+
